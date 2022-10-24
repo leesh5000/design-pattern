6cbd3a16ba43917ed2c7c802554248383a880332
leesh5000 <leesh50000@gmail.com>
2022-10-25 03:22:27 +0900

    클라이언트 코드 변경

--- creational/src/abstractfactory/Client.java
+++ creational/src/abstractfactory/Client.java
@@ -1,24 +1,29 @@
 package abstractfactory;
 
+import abstractfactory.abstractfactory.UIMaker;
 import abstractfactory.abstractproduct.ScrollBar;
 import abstractfactory.abstractproduct.Window;
 import abstractfactory.concretefactory.AppleUIMaker;
-import abstractfactory.abstractfactory.UIMaker;
+import abstractfactory.concretefactory.WindowsUIMaker;
 
 public class Client {
 
     public static void main(String[] args) {
+        Client client1 = new Client();
+        client1.logic(new AppleUIMaker());
 
-        UIMaker uiMaker = new AppleUIMaker();
+        Client client2 = new Client();
+        client2.logic(new WindowsUIMaker());
+    }
 
+    // 사용자에서 실제 실행할 비즈니스 로직 부분
+    private void logic(UIMaker uiMaker) {
         // 사용자는 UIMaker의 구체적인 서브클래스가 무엇인지 알 필요가 없다.
         // 단지, UIMaker에 자신이 필요로하는 명령만 전달해주면 된다.
-        Window window = uiMaker.createWindow();
         ScrollBar scrollBar = uiMaker.createScrollBar();
-
+        Window window = uiMaker.createWindow();
         System.out.println(window.getClass());
         System.out.println(scrollBar.getClass());
-
     }
 
 }
--- creational/src/factorymethod/Client.java
+++ creational/src/factorymethod/Client.java
@@ -2,16 +2,27 @@ package factorymethod;
 
 import factorymethod.concretecreator.ExcelFactory;
 import factorymethod.concretecreator.PdfFactory;
-import factorymethod.concreteproduct.Excel;
-import factorymethod.concreteproduct.Pdf;
+import factorymethod.creator.DocumentFactory;
+import factorymethod.product.Document;
 
 public class Client {
+
     public static void main(String[] args) {
-        Pdf pdf = new PdfFactory().create();
-        Excel excel = new ExcelFactory().create();
+
+        Client client1 = new Client();
+        client1.logic(new PdfFactory());
+
+        Client client2 = new Client();
+        client2.logic(new ExcelFactory());
 
         // 새로운 요구사항이 생기면 기존 코드를 변경하지 않고
         // ConcreteCreator, ConcreteProduct를 확장해주기만 하면 된다.
         // ...
+
+    }
+
+    private void logic(DocumentFactory documentFactory) {
+        Document document = documentFactory.create();
+        System.out.println(document.getClass());
     }
 }
